64310c384f0abc3f43318c6feeba648acb22ee63
blinkosm10 <rico2clay@gmail.com>
2025-09-14 21:56:23 +0100

    Update idvb.h

--- lib/dvb/idvb.h
+++ lib/dvb/idvb.h
@@ -368,6 +368,8 @@ public:
 	/* m_service_name_sort is uppercase, with special chars removed, to increase sort performance. */
 	std::string m_service_name, m_service_name_sort;
 	std::string m_provider_name;
+    std::string m_default_authority;
+	uint32_t m_aus_da_flag;
 	int m_lcn;
 	int getLCN() { return m_lcn; }
 
